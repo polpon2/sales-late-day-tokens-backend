709bc84f0d763cc0f2f366b922d4a9a879770fd0
danamestrial <chayapolbun@gmail.com>
2023-11-30 22:49:17 +0700

    feat: opentele support

--- app/routers/api_purchase.py
+++ app/routers/api_purchase.py
@@ -11,13 +11,22 @@ load_dotenv()
 
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
-from opentelemetry.sdk.resources import SERVICE_NAME, Resource
-from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
+from opentelemetry.sdk.trace.export import (
+    BatchSpanProcessor,
+    ConsoleSpanExporter,
+)
 
-
+# Sets provider
 provider = TracerProvider()
+
+# Sets processor for span
+processor = BatchSpanProcessor(ConsoleSpanExporter())
+provider.add_span_processor(processor)
+
+# Sets the global default tracer provider
 trace.set_tracer_provider(provider)
+
+# Creates a tracer from the global tracer provider
 tracer = trace.get_tracer(__name__)
 
 router = APIRouter(prefix="/api")
@@ -32,25 +41,8 @@ def get_db():
 
 @router.post("/purchase/")
 async def purchase(purchase_detail: Purchase):
-
-    with tracer.start_as_current_span("kick-start-from-backend"):
-        carrier = {}
-
-       
-        # Write the current context into the carrier.
-        TraceContextTextMapPropagator().inject(carrier)
-        send_data = {
-            # username: str
-            # price: int
-            # amount: int
-            "username": purchase_detail.username,
-            "price": purchase_detail.price,
-            "amount": purchase_detail.amount,
-            "traceparent": carrier['traceparent']
-        }
-
-        rabbitmq.send_data(queue_name='from.backend', data=(json.dumps(send_data)))
-    return    
+    rabbitmq.send_data(queue_name='from.backend', data=(json.dumps(purchase_detail.model_dump())))
+    return
 
 @router.post("/deliver-completed")
 async def processComplete(purchase_detail: Purchase):
